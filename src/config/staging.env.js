c0cc0351d1f1ceb3cc12be9ed2f4e7a84fc72aa5
bistmaster <bethoveen.todino@gmail.com>
2018-09-14 15:02:50 +0800

    removing the staging config file

--- src/config/staging.env.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-  username: process.env.NEO4J_USERNAME || 'neo4j',
-  password: process.env.NEO4J_PASSWORD || 'qwerty123',
-  url: process.env.NEO4J_HOST || 'bolt://localhost'
-} 
